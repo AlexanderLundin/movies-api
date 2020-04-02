d14e484ba3af135ca3cb7124a3091575060242e1
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-01 18:09:57 -0700

    added test for parameterConstructor on movie entity

--- src/main/java/com/galvanize/entities/Movie.java
+++ src/main/java/com/galvanize/entities/Movie.java
@@ -1,4 +1,26 @@
 package com.galvanize.entities;
 
+import java.time.LocalDate;
+
 public class Movie {
+    Long movieid;
+    String imdbid;
+    String actors;
+    String director;
+    String title;
+    String year;
+    LocalDate released;
+
+    public Movie() {
+    }
+
+    public Movie(String imdbid, String actors, String director, String title, String year, LocalDate released) {
+        this.imdbid = imdbid;
+        this.actors = actors;
+        this.director = director;
+        this.title = title;
+        this.year = year;
+        this.released = released;
+    }
+
 }
--- src/test/java/com/galvanize/entities/MovieTest.java
+++ src/test/java/com/galvanize/entities/MovieTest.java
@@ -2,15 +2,33 @@ package com.galvanize.entities;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
-    
+
     @Test
     public void defaultConstructor_returnsMovie() {
         //Setup
         //Exercise
         Movie movie = new Movie();
+        //Assert
+        assertNotNull(movie);
+        //Teardown
+    }
+
+    @Test
+    public void parameterConstructor_returnsMovie() {
+        //Setup
+        String imdbid = "1";
+        String actors = "actor1, actor2, actor3";
+        String director = "director";
+        String title = "title";
+        String year = "2020";
+        LocalDate released = LocalDate.now();
+        //Exercise
+        Movie movie = new Movie(imdbid, actors, director, title, year, released);
         //Assert
         assertNotNull(movie);
         //Teardown
